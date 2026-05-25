04c1a861e5aa4249f128965bdea9fa5d7753cec8
lucas <lucasfernandesabe@gmail.com>
2026-05-24 23:05:34 -0300

    Alterado exibição de itens no light mode

--- assets/script.js
+++ assets/script.js
@@ -26,10 +26,17 @@ hamburgerButton.addEventListener("click", () => {
 
 })
 
-const toggleButton = document.querySelector("#toggle-mode");
-const toggleButtonIcon = document.querySelector("#toggle-mode i");
+const toggleButton = document.querySelector(".toggle-mode");
+const toggleButtonIcon = document.querySelector(".toggle-mode i");
 const toggleButtonText = document.querySelector("#toggle-container span");
 const body = document.querySelector("body");
+
+const navbars = document.querySelectorAll(".navbar-link");
+
+const servicoItems = document.querySelectorAll(".servico");
+
+const contatos = document.querySelectorAll(".contato-item");
+const contatoWhatsApp = document.getElementById("contato-whatsapp");
 
 toggleButton.addEventListener("click", () => {
 
@@ -38,12 +45,52 @@ toggleButton.addEventListener("click", () => {
         toggleButtonIcon.classList.add("fa-toggle-on");
         toggleButtonText.textContent = "Light Mode";
         body.classList.add("white-mode");
+        toggleButton.classList.add("black-button");
+        navbars.forEach(
+            (navbar) => {
+                navbar.classList.add("black-container");
+            }
+        )
+        servicoItems.forEach(
+            (servico) => {
+                servico.classList.remove("servico");
+                servico.classList.add("servico2");
+            }
+        )
+        contatos.forEach(
+            (contato) => {
+                contato.classList.add("contato-item2");
+            }
+        )
+        contatoWhatsApp.removeAttribute("id", "contato-whatsapp");
+        contatoWhatsApp.setAttribute("id", "contato-whatsapp2");
+        hamburgerButton.style.border = "1px solid black";
     }
     else {
         toggleButtonIcon.classList.remove("fa-toggle-on");
         toggleButtonIcon.classList.add("fa-toggle-off");
         toggleButtonText.textContent = "Dark Mode";
         body.classList.remove("white-mode");
+        toggleButton.classList.remove("black-button");
+        navbars.forEach(
+            (navbar) => {
+                navbar.classList.remove("black-container");
+            }
+        )
+        servicoItems.forEach(
+            (servico) => {
+                servico.classList.add("servico");
+                servico.classList.remove("servico2");
+            }
+        )
+        contatos.forEach(
+            (contato) => {
+                contato.classList.remove("contato-item2");
+            }
+        )
+        contatoWhatsApp.removeAttribute("id", "contato-whatsapp2");
+        contatoWhatsApp.setAttribute("id", "contato-whatsapp");
+        hamburgerButton.style.border = "1px solid white";
     }
 
 })
